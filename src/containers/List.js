ac42348494c62fe1b892b3c4a928d1a70a8a26d1
John Whiteside <johnwhitesideii@gmail.com>
2017-03-07 21:38:42 -0500

    Add Decorators and Refactor List Container
    Added babel plugin for decorators and refactored the
    ListContainer to use a decorator.

--- src/containers/List.js
+++ src/containers/List.js
@@ -6,26 +6,6 @@ import AddItemForm from '../components/AddItemForm';
 import Divider from 'material-ui/Divider';
 import sortByActive from '../utils/sortByActive';
 
-class ListContainer extends React.Component {
-
-  render(){
-    let { add, remove, items, complete, setActive } = this.props;
-    let listProps = {
-      items: sortByActive(items),
-      remove,
-      complete,
-      setActive
-    };
-    return (
-      <div>
-        <AddItemForm add={add} />
-        <Divider />
-        <List {...listProps} />
-      </div>
-    );
-  }
-}
-
 const mapStateToProps = (state) => ({
   items: state.list
 })
@@ -45,4 +25,23 @@ const dispatchToProps = (dispatch) => ({
   }
 });
 
-export default connect(mapStateToProps, dispatchToProps)(ListContainer);
+@connect(mapStateToProps, dispatchToProps)
+export default class ListContainer extends React.Component {
+
+  render(){
+    let { add, remove, items, complete, setActive } = this.props;
+    let listProps = {
+      items: sortByActive(items),
+      remove,
+      complete,
+      setActive
+    };
+    return (
+      <div>
+        <AddItemForm add={add} />
+        <Divider />
+        <List {...listProps} />
+      </div>
+    );
+  }
+}
